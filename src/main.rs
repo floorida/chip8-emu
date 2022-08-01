22e022c2ecfe212e41c710e6fbfdfcde11239303
florida <85819739+floorida@users.noreply.github.com>
2022-08-01 03:37:03 -0700

    added sprites

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let mut f = File::open("INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
     f.read_to_end(&mut data).unwrap();
-    println!("SPCINV: {:?}", data);
+    // println!("SPCINV: {:?}", data);
 
     let mut chip8 = Chip8::new();
     chip8.load_rom(&data);
