e0e32912a06cb4a77c57a4666dcbd2a2ecd884e7
florida <85819739+floorida@users.noreply.github.com>
2022-08-01 01:01:28 -0700

    added master

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::prelude::*;
+use crate::chip8::*;
+
+pub mod ram;
+pub mod chip8;
+
+fn main() {
+    let mut f = File::open("INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    f.read_to_end(&mut data).unwrap();
+    println!("SPCINV: {:?}", data);
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
+}
